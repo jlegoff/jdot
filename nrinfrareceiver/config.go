5170cf06c39c610f5e13122b605a1515459f2344
Julien Le Goff <jlegoff@newrelic.com>
2023-02-22 11:48:03 +0100

    fix bogus config validation

--- nrinfrareceiver/config.go
+++ nrinfrareceiver/config.go
@@ -1,19 +1,15 @@
 package nrinfrareceiver
 
-import (
-	"fmt"
-	"os"
-)
-
 type Config struct {
 	AgentConfigPath string `mapstructure:"agent_config_path"`
 }
 
 func (cfg *Config) Validate() error {
-	if cfg.AgentConfigPath != "" {
-		if _, err := os.Stat(cfg.AgentConfigPath); os.IsNotExist(err) {
-			return fmt.Errorf("Unable to access Infra agent path: %s", cfg.AgentConfigPath)
-		}
-	}
+	// not actually used
+	//if cfg.AgentConfigPath != "" {
+	//	if _, err := os.Stat(cfg.AgentConfigPath); os.IsNotExist(err) {
+	//		return fmt.Errorf("Unable to access Infra agent path: %s", cfg.AgentConfigPath)
+	//	}
+	//}
 	return nil
 }
