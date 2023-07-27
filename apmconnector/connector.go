8f9e359726cf666c85739c02410aca9577fb92dc
Julien Le Goff <jlegoff@newrelic.com>
2023-07-27 13:05:59 +0200

    First implementation

--- apmconnector/connector.go
+++ apmconnector/connector.go
@@ -2,8 +2,10 @@ package apmconnector
 
 import (
 	"context"
+	"fmt"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.uber.org/zap"
 )
@@ -19,8 +21,9 @@ func (c *ApmConnector) Capabilities() consumer.Capabilities {
 	return consumer.Capabilities{MutatesData: false}
 }
 
-func (c *ApmConnector) ConsumeTraces(ctx context.Context, md ptrace.Traces) error {
-	return nil
+func (c *ApmConnector) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
+	metrics := ConvertTraces(td)
+	return c.metricsConsumer.ConsumeMetrics(ctx, metrics)
 }
 
 func (c *ApmConnector) Start(_ context.Context, host component.Host) error {
@@ -31,4 +34,45 @@ func (c *ApmConnector) Start(_ context.Context, host component.Host) error {
 func (c *ApmConnector) Shutdown(context.Context) error {
 	c.logger.Info("Stopping the APM Connector")
 	return nil
+}
+
+func ConvertTraces(td ptrace.Traces) pmetric.Metrics {
+	metrics := pmetric.NewMetrics()
+	for i := 0; i < td.ResourceSpans().Len(); i++ {
+		resourceMetrics := metrics.ResourceMetrics().AppendEmpty()
+		rs := td.ResourceSpans().At(i)
+		rs.Resource().CopyTo(resourceMetrics.Resource())
+		for j := 0; j < rs.ScopeSpans().Len(); j++ {
+			scopeSpan := rs.ScopeSpans().At(j)
+			scopeMetric := resourceMetrics.ScopeMetrics().AppendEmpty()
+			for k := 0; k < scopeSpan.Spans().Len(); k++ {
+				span := scopeSpan.Spans().At(k)
+				if span.Kind() != ptrace.SpanKindServer {
+					continue
+				}
+				metric := scopeMetric.Metrics().AppendEmpty()
+				metric.SetName("apm.service.transaction.duration")
+				metric.SetUnit("s")
+
+				dp := metric.SetEmptyHistogram().DataPoints().AppendEmpty()
+				dp.SetStartTimestamp(span.StartTimestamp())
+				dp.SetTimestamp(span.EndTimestamp())
+
+				duration := float64((span.EndTimestamp() - span.StartTimestamp()).AsTime().UnixNano()) / 1e9
+				dp.SetSum(duration)
+				dp.SetCount(1)
+				httpRoute, routePresent := span.Attributes().Get("http.route")
+				if routePresent {
+					method, methodPresent := span.Attributes().Get("http.request.method")
+					if methodPresent {
+						dp.Attributes().PutStr("transactionName", fmt.Sprintf("WebTransaction/http.route/%s %s", httpRoute.Str(), method.Str()))
+					} else {
+						dp.Attributes().PutStr("transactionName", fmt.Sprintf("WebTransaction/http.route/%s", httpRoute.Str()))
+					}
+				}
+			}
+		}
+
+	}
+	return metrics
 }
